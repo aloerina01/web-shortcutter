05f0652899d4ee44feb301b427daa74c856f9c17
あろえりーな <kiss_mint27@yahoo.co.jp>
2016-06-13 17:36:22 +0900

    add task to compile Less

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
 var gulpCoffee = require('gulp-coffee');
+var less = require('gulp-less');
 
 gulp.task('coffee', function(){
   gulp.src('lib/*')
     .pipe(gulpCoffee());
+});
+
+gulp.task('less', function(){
+  gulp.src('./styles/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./mock'));
+});
+
+gulp.task('default', function(){
+  gulp.watch('styles/*.less', ['less']);
 });
